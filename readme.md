25a26c1395b3a0be5f1bf3ea9bc2b4f6e5213957
AlexanderHBC <aikadxuk@gmail.com>
2024-03-03 16:56:54 -0300

    Projeto finalizado, ainda falta uns ajustes na responsividade de 540px pra baixo, mas tudo está funcional

---
+++ readme.md
@@ -0,0 +1,10 @@
+//Criei uma calculadora de gorjetas
+//Você deve digitar o valor total da conta na área Bill
+//Depois digitar o número de pessoas na área Number of People
+//Aí aqui tem duas opções:
+//Você pode clicar em um dos botões verdes que farão o cálculo pelas porcentagens pré-definidas
+//Ou você pode digitar um valor no campo com uma porcentagem que você quer
+//O cálculo é automático e os resultados aparecem na área verde
+//Por fim, se quiser apagar os valores do resultado, é só clicar no botão Reset
+
+//See ya!
--- script.js
+++ script.js
@@ -1,83 +1,81 @@
-let cash
-let people
-let fivePercent = document.querySelector('.button-1').addEventListener('click', fiveCalculate)
-let tenPercent = document.querySelector('.button-2').addEventListener('click', tenCalculate)
-let fifteenPercent = document.querySelector('.button-3').addEventListener('click', fifteenCalculate)
-let twentyfivePercent = document.querySelector('.button-4').addEventListener('click', twentyfiveCalculate)
-let fiftyPercent = document.querySelector('.button-5').addEventListener('click', fiftyCalculate)
-let custom = document.querySelector('.button-6')
-const reset = document.querySelector('#reset').addEventListener('click', resetButton)
-let tipAmount = document.querySelector('#peopleamount-tip')
-let totalAmount = document.querySelector('#peopletotal-tip')
+const valueToCalculate = document.querySelector('#value-to-calculate')
+const numberOfPeople = document.querySelector('#number-of-people')
 
-function fiveCalculate(){
-     cash = document.querySelector('.cash').value
-     people = document.querySelector('.people').value
+const buttonsPercent = document.querySelectorAll('.percent-button')
+const customPercent = document.querySelector('.custom-percent-input')
 
-     let calculateEach = cash / people * 5 / 100
-     calculateEach = calculateEach.toFixed(2)
-     tipAmount.innerHTML = `$${calculateEach}`
+const totalTipResult = document.querySelector('#amount-person-result')
+const totalTipPerPerson = document.querySelector('#total-person-result')
 
-     let calculateTotal = cash / people * 5 / 100 * 5
-     calculateTotal = calculateTotal.toFixed(2)
-     totalAmount.innerHTML = `$${calculateTotal}`
-}
+const resetValues = document.querySelector('.reset-values')
 
-function tenCalculate(){
-     cash = document.querySelector('.cash').value
-     people = document.querySelector('.people').value
+let totalTip
+let roundedValue
+let roundedTotalTipValue
+let totalPercent
 
-     let calculateEach = cash / people * 10 / 100
-     calculateEach = calculateEach.toFixed(2)
-     tipAmount.innerHTML = `$${calculateEach}`
+buttonsPercent.forEach(function (button, index) {
+     button.addEventListener('click', () => {
+          let totalValue = valueToCalculate.value
+          let totalPerson = numberOfPeople.value
+          if (index === 0 && totalValue > 0 && totalPerson > 0) {
+               totalTip = 5 / 100 * totalValue
+               roundedTotalTipValue = totalTip.toFixed(2)
+               totalTipResult.innerHTML = `$ ${roundedTotalTipValue}`
+               totalPercent = totalTip / totalPerson
+               roundedValue = totalPercent.toFixed(2)
+               totalTipPerPerson.innerHTML = `$ ${roundedValue}`
+          } else if (index === 1 && totalValue > 0 && totalPerson > 0) {
+               totalTip = 10 / 100 * totalValue
+               roundedTotalTipValue = totalTip.toFixed(2)
+               totalTipResult.innerHTML = `$ ${roundedTotalTipValue}`
+               let totalPercent = totalTip / totalPerson
+               roundedValue = totalPercent.toFixed(2)
+               totalTipPerPerson.innerHTML = `$ ${roundedValue}`
+          } else if (index === 2 && totalValue > 0 && totalPerson > 0) {
+               totalTip = 15 / 100 * totalValue
+               roundedTotalTipValue = totalTip.toFixed(2)
+               totalTipResult.innerHTML = `$ ${roundedTotalTipValue}`
+               totalPercent = totalTip / totalPerson
+               roundedValue = totalPercent.toFixed(2)
+               totalTipPerPerson.innerHTML = `$ ${roundedValue}`
+          } else if (index === 3 && totalValue > 0 && totalPerson > 0) {
+               totalTip = 25 / 100 * totalValue
+               roundedTotalTipValue = totalTip.toFixed(2)
+               totalTipResult.innerHTML = `$ ${roundedTotalTipValue}`
+               totalPercent = totalTip / totalPerson
+               roundedValue = totalPercent.toFixed(2)
+               totalTipPerPerson.innerHTML = `$ ${roundedValue}`
+          } else if (index === 4 && totalValue > 0 && totalPerson > 0) {
+               totalTip = 50 / 100 * totalValue
+               roundedTotalTipValue = totalTip.toFixed(2)
+               totalTipResult.innerHTML = `$ ${roundedTotalTipValue}`
+               totalPercent = totalTip / totalPerson
+               roundedValue = totalPercent.toFixed(2)
+               totalTipPerPerson.innerHTML = `$ ${roundedValue}`
+          }
+     })
+})
 
-     let calculateTotal = cash / people * 10 / 100 * 5
-     calculateTotal = calculateTotal.toFixed(2)
-     totalAmount.innerHTML = `$${calculateTotal}`
-}
+customPercent.addEventListener('keyup', () => {
+     let valueToCalculateInput = valueToCalculate.value
+     let totalPersonInput = numberOfPeople.value
+     let customPercentInput = customPercent.value
+     if (customPercentInput > 0 && valueToCalculateInput > 0 && totalPersonInput > 0) {
+          totalTip = customPercentInput / 100 * valueToCalculateInput
+          roundedTotalTipValue = totalTip.toFixed(2)
+          totalTipResult.innerHTML = `$ ${roundedTotalTipValue}`
+          totalPercent = totalTip / totalPersonInput
+          roundedValue = totalPercent.toFixed(2)
+          totalTipPerPerson.innerHTML = `$ ${roundedValue}`
+     }
+})
 
-function fifteenCalculate(){
-     cash = document.querySelector('.cash').value
-     people = document.querySelector('.people').value
-
-     let calculateEach = cash / people * 15 / 100
-     calculateEach = calculateEach.toFixed(2)
-     tipAmount.innerHTML = `$${calculateEach}`
-
-     let calculateTotal = cash / people * 15 / 100 * 5
-     calculateTotal = calculateTotal.toFixed(2)
-     totalAmount.innerHTML = `$${calculateTotal}`
-}
-
-function twentyfiveCalculate(){
-     cash = document.querySelector('.cash').value
-     people = document.querySelector('.people').value
-
-     let calculateEach = cash / people * 25 / 100
-     calculateEach = calculateEach.toFixed(2)
-     tipAmount.innerHTML = `$${calculateEach}`
-
-     let calculateTotal = cash / people * 25 / 100 * 5
-     calculateTotal = calculateTotal.toFixed(2)
-     totalAmount.innerHTML = `$${calculateTotal}`
-}
-
-function fiftyCalculate(){
-     cash = cash = document.querySelector('.cash').value
-     people = document.querySelector('.people').value
-
-     let calculateEach = cash / people * 50 / 100
-     calculateEach = calculateEach.toFixed(2)
-     tipAmount.innerHTML = `$${calculateEach}`
-
-     let calculateTotal = cash / people * 50 / 100 * 5
-     calculateTotal = calculateTotal.toFixed(2)
-     totalAmount.innerHTML = `$${calculateTotal}`
-}
-
-function resetButton(){
-     cash = document.querySelector('.cash').value = ''
-     people = document.querySelector('.people').value = ''
-     tipAmount.innerHTML = `$0.00`
-     totalAmount.innerHTML = `$0.00`
-}
+resetValues.addEventListener('click', () => {
+     let inputPeopleReset = numberOfPeople.value
+     let totalValueReset = valueToCalculate.value
+     if (inputPeopleReset > 0 || totalValueReset > 0) {
+          totalTipResult.innerHTML = `$0.00`
+          totalTipPerPerson.innerHTML = `$0.00`
+     }
+})
